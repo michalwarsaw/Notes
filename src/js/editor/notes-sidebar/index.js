61e01c940745891199e3aafd640399d5f9220aa5
Michal Wlodarczyk <polskito@gmail.com>
2018-10-22 16:37:14 +0200

    Update

--- src/js/editor/notes-sidebar/index.js
+++ src/js/editor/notes-sidebar/index.js
@@ -19,14 +19,18 @@ const notesList = [
     },
 ];
 
-const generateNoteHTML = note => {
-    return `<li class="list-group-item d-flex justify-content-between lh-condensed">
-    <div>
-       <h6 class="my-0">${note.title}</h6>
-       <small class="text-muted">${note.lastModified}</small>
-    </div>
-    </li>`;
-};
+const generateNoteList = () => {
 
-let html = notesList.map(note => generateNoteHTML(note)).join(''); // join('') is deleting the "," between <li>;
-notesListEl.innerHTML = html;
+    const generateNoteHTML = note => {
+        return `<li class="list-group-item d-flex justify-content-between lh-condensed">
+        <div>
+           <h6 class="my-0">${note.title}</h6>
+           <small class="text-muted">${note.lastModified}</small>
+        </div>
+        </li>`;
+    };
+    
+    const html = notesList.map(note => generateNoteHTML(note)).join(''); // join('') is deleting the "," between <li>;
+    notesListEl.innerHTML = html;
+};
+generateNoteList();
