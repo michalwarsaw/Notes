ba58b7b76f9d3713e0fa5d254faf140b97733ee2
Michal Wlodarczyk <polskito@gmail.com>
2018-10-24 22:53:51 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3000'
+
 import './scss/style.scss';
 import './js/app';
--- src/js/editor/notes-actions.js
+++ src/js/editor/notes-actions.js
@@ -1,53 +1,37 @@
-let notesList = [
-    {
-        id: 1,
-        title: 'Note 1',
-        body: '# sample body 1',
-        lastModified: 1540326489711
-    },
-    {
-        id: 2,
-        title: 'Note 2',
-        body: '# sample body 2',
-        lastModified: 1540326489711
-    },
-    {
-        id: 3,
-        title: 'Note 3',
-        body: '# sample body 3',
-        lastModified: 1540326489711
-    },
-    {
-        id: 4,
-        title: 'Note 4',
-        body: '# sample body 4',
-        lastModified: 1540326489711
-    },
-];
+import axios from 'axios';
+
+axios.get('/notes')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+
+let notesList = [];
 
 export let getAll = () => notesList;
-export let add = title => notesList.push({
-    title,
-    lastModified: Date.now()
-});
+export let add = (title) =>
+	notesList.push({
+		title,
+		lastModified: Date.now()
+	});
 
 // Remove
 
 export let remove = (noteId) => {
-    notesList = notesList.filter(note => note.id !== noteId) // returning the new noteList without note with noteId from parameter.
-    console.log(`${noteId} - removed`);
-    console.log(notesList);
-}
+	notesList = notesList.filter((note) => note.id !== noteId); // returning the new noteList without note with noteId from parameter.
+	console.log(`${noteId} - removed`);
+	console.log(notesList);
+};
 
 // Save
 
 export let save = (noteId, noteContent) => {
-    notesList.map(note => {
-        if (note.id === noteId) {
-            note.body = noteContent;
-            note.lastModified = Date.now();
-        }
-        
-        return note;
-    })
-}
+	notesList.map((note) => {
+		if (note.id === noteId) {
+			note.body = noteContent;
+			note.lastModified = Date.now();
+		}
+
+		return note;
+	});
+};
